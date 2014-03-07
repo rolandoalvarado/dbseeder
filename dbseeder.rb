dfb15a6202f30a3c9c539733a54f1f68c3facd81
Rolando Alvarado <rorroland@gmail.com>
2014-03-07 13:31:47 +0800

    Updated Gemspec.

--- dbseeder.rb
+++ dbseeder.rb
@@ -1,4 +1,3 @@
-# Main Class for DBSeeder
 class dbseeder
   def self.create(adapter='', db_name='', db_user='', db_pass='', username='', password='')
     connection = Connection.new(adapter, db_name, db_user, db_pass)
